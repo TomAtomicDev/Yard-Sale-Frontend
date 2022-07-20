48b707b5fcc6b9c5ec8349fa735d8d7b59e364fd
TomAtomicDev <tompenaloza@gmail.com>
2022-07-19 22:40:05 -0400

    Implemented react hooks

--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -3,7 +3,7 @@ import './ProductItem.scss';
 
 import iconAddCart from '@icons/bt_add_to_cart.svg'; 
 
-function ProductItem(props) {
+function ProductItem({product}) {
   const [cart, setCart]= useState([]);
 
   const handleClick = (event) => {
@@ -11,11 +11,11 @@ function ProductItem(props) {
   }
   return (
     <div className="product-item">
-      <img src="https://images.pexels.com/photos/844734/pexels-photo-844734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt className="product-item__img" />
+      <img src={product.images[0]} alt={product.title} className="product-item__img" />
       <div className="item-info">
         <div className="item-text">
-          <p className="item-text__price">$129,00</p>
-          <p className="item-text__name">Kindle Paperwhite 10th Gen</p>
+          <p className="item-text__price">${product.price}</p>
+          <p className="item-text__name">{product.title}</p>
         </div>
         <figure onClick={handleClick} className="item-info__icon">
           <img src={iconAddCart} alt />
--- src/containers/ProductList/index.jsx
+++ src/containers/ProductList/index.jsx
@@ -1,28 +1,19 @@
-import React, {useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import {ProductItem} from '../../components/ProductItem';
+import useGetProducts from '../../hooks/useGetProducts'
 import './ProductList.scss';
 
 const API = 'https://api.escuelajs.co/api/v1/products';
 
 function ProductList (props) {
 
-  const [products, setProducts] = useState([]);
-
-  useEffect( () => {
-		const getProducts = async () => {
-			const response = await axios(API);
-      console.log(response.data);
-			setProducts(response.data);
-		};
-		getProducts();
-  }, [])
+  const products = useGetProducts(API);
 
   return (
     <section className="main-container">
       <div className="cards-container">
         {products.map( product => {
-          return <ProductItem key={ product.id }/>
+          return <ProductItem product={product} key={ product.id }/>
         }) }
 
       </div>
